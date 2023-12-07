46f4247bfb12695dbeebac39367f205ef04f0150
[name] <jaipal@createbytes.com>
2023-12-07 12:16:38 +0530

    express ts sever setup conmpleted with web socket

---
+++ Server/src/index.ts
@@ -0,0 +1,30 @@
+import express,{Request,Response, Application} from "express";
+import { createServer } from "http";
+import {Server} from "socket.io"
+// import { Server } from "http";
+
+const app : Application = express();
+const PORT = 8585;
+
+app.use(express.json());
+
+
+app.get("/",(req : Request, res : Response)=>{
+    res.status(200).send(`Welcome to Chat app `)
+});
+
+const server = createServer(app);
+const io = new Server(server);
+io.on("connection",(socket)=>{
+    console.log("Connection done.");
+    socket.send("Hey Clinet, How are you? \n from server!")
+    socket.on("chat message",(msg)=>{
+        console.log(`${msg} Thanks for messageing me!`);
+    })
+})
+
+
+app.listen(PORT, async()=>{
+    console.log({Server : `Listing on Port ${PORT} 🤙`})
+})
+
